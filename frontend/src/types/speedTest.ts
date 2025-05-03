0183bbc8de6c7ec630a66e72efedb557b3e7e20f
Colin Palmer <11085027+colpalm@users.noreply.github.com>
2025-05-03 13:19:43 -0400

    ist-18 Add Prettier integration for frontend code formatting
    Applying prettier changes.

--- frontend/src/types/speedTest.ts
+++ frontend/src/types/speedTest.ts
@@ -4,7 +4,7 @@ export interface SpeedTestResult {
   upload_speed: number;
   latency: number;
   time_of_day: string;
-  server: {name: string, url: string};
+  server: { name: string; url: string };
 }
 
 export interface SpeedTestChartData extends SpeedTestResult {
